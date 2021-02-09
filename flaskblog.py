2354028d089785810a5960b66a086f473605c11a
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2021-02-10 01:32:21 +0530

    added html forms, login and register page.

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from form import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '78e88e5bd9a75d2122e757aa130020be' # Provides a protection from hackers
 
 posts = [
     {
@@ -25,6 +28,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html", title="About")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for('home')) # url_for(arg), here arg is function name, not route name.
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been successfully logged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login unsuccessful. Please check username and password.", "danger")
+    return render_template('login.html', title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
