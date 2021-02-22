42e4c16cfaed8e2705fa447d677ffd5cb67aaa58
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2021-02-22 20:19:32 +0530

    account info, update account info

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
+import secrets
+import os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog.models import User, Post
-from flaskblog.form import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -65,8 +68,52 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    # html form will add the 'filename' attribute to the form_picture value automatically.
+    # os.path.split will give two values viz. file_name and file_extension, but here we just want to use the extension, so in python if we donot want to use a variable, we name it as underscore('_') like we did here with filename
+    picture_fn = random_hex + f_ext # picture_filename
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    # resizing the images to 125 px, else large images will take much space on the file system
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+def delete_picture(image_file):
+    image_path = os.path.join(app.root_path, 'static/profile_pics', image_file)
+    os.remove(image_path)
+
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required # look for login_manager.login_view in __init__.py . Try to access this page without logging in and it will redirect to the login page and add a "next" parameter in the url
 def account():
-    return render_template('account.html', title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            if current_user.image_file != 'default.jpg':
+                delete_picture(current_user.image_file)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image = url_for('static', filename='profile_pics/' + current_user.image_file)
+
+    # if file not present
+    if not os.path.exists(image):
+        image = url_for('static', filename='profile_pics/' + 'default.jpg')
+    return render_template('account.html', title="Account", image_file=image, form=form)
 
